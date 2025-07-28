719ba1f9e4a0c68d37288fc578aedcfd13603ff4
Pankaj Kain <kainpankaj26@gmail.com>
2025-07-28 10:58:08 +0530

    Todo file added

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ ec2_client =aws_managment.client('ec2')
 #print("This is client Response : ",dir(ec2_client)) 
  
 print() 
-ec2_client.describe_regions()
-
+all_region = ec2_client.describe_regions()
+print(all_region)
 
 # resource
 ec2_resource = aws_managment.resource('ec2') 
+print(ec2_resource.describe_regions())
 #print("This is Resource Response",dir(ec2_resource))
